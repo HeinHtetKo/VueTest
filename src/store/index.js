d2b3d9eaae0b259e37df17036bcf8d343ae8472c
Hein Htet Ko <heinhtetko.cudawei@gmail.com>
2021-10-27 16:30:48 +0630

    adding new codes

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from "vuex";
+import Vue from "vue";
+import ToDos from "./modules/ToDos";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: { ToDos },
+});
---
+++ src/store/modules/ToDos.js
@@ -0,0 +1,8 @@
+export default {
+  state: {
+    sayHello: "Hello World!!!",
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+};
